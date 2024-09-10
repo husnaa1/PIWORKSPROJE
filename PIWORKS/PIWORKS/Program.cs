c4d17a72884b5362242cc0d61034661c7ce21d44
Fatma Hüsna Kılıç <husnaklc6167@gmail.com>
2024-09-10 13:24:01 +0300

    PIWORKS-prime numbers
    prime numbers having 3 digits and starting with 5

---
+++ PIWORKS/PIWORKS/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//all prime numbers having 3 digits and starting with 5
+
+
+Console.Write("5 ile başlayan 3 basamaklı tüm asal sayılar \n");
+
+int sayac;
+
+    for (int i = 500; i < 600; i++)
+    {
+        sayac = 0;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                sayac++;
+            }
+        }
+        if (sayac == 0)
+        {
+            Console.WriteLine("Asal Sayılar = " + i);
+        }
+    }
+    Console.ReadLine();
